7aa126a1b420da878d075313cb6617db5ab81ea4
MajortomVR <majortom.rb@gmail.com>
2022-08-09 15:37:00 +0200

    improved: Timer calculation

--- timer.js
+++ timer.js
@@ -1,35 +1,37 @@
 var Timer = class Timer {
    constructor() {
       this.running = false;
-      this.lastUpdateTimestamp = new Date().getTime();
-      this.timeLeftSeconds = 0;
       this.finished = false;  // Finished is true when the timer has run until 0, but not when it got stopped.
+      this.startTimestamp = 0;
+      this.duration = 0;
+      this.timeLeftSeconds = 0;
    }   
 
    start(timeSeconds) {
       this.running = true;
-      this.lastUpdateTimestamp = new Date().getTime();
-      this.timeLeftSeconds = timeSeconds;      
       this.finished = false;
+      this.startTimestamp = new Date().getTime();
+      this.duration = timeSeconds;
+      this.timeLeftSeconds = timeSeconds;
    }
 
    reset() {
       this.running = false;
-      this.timeLeftSeconds = 0;
       this.finished = false;
+      this.startTimestamp = 0;
+      this.timeLeftSeconds = 0;
+      this.duration = 0;      
    }
    
    update() {
       if (this.running && this.timeLeftSeconds <= 0) {
          this.running = false;
+         this.startTimestamp = 0;
          this.timeLeftSeconds = 0;
          this.finished = true;
       }
-
-      let timeElapsed = new Date().getTime() - this.lastUpdateTimestamp;
-      this.lastUpdateTimestamp = new Date().getTime();
-
-      this.timeLeftSeconds -= timeElapsed / 1000;
-      this.timeLeftSeconds = Math.max(this.timeLeftSeconds, 0)
+      
+      let timeElapsed = (new Date().getTime() - this.startTimestamp) / 1000;
+      this.timeLeftSeconds = Math.max(this.duration - timeElapsed, 0);
    }
 }
